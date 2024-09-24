83abfc9faa6b8062838370a7834da63542652896
mr_nobody <mahfuzantor2@gmail.com>
2024-09-25 01:31:03 +0600

    meeting routes created and connected with clientcide

--- models/mongoDb.js
+++ models/mongoDb.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pgsiu4c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 let userCollection; // Define a variable to store the user collection
+let meetingCollection; // Define a variable to store the meeting collection
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,6 +20,7 @@ async function run() {
         await client.connect();
         const database = client.db("meetUp")
         userCollection = database.collection("users")
+        meetingCollection = database.collection("meetings")
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -31,4 +33,5 @@ run().catch(console.dir);
 
 module.exports = {
     getUserCollection: () => userCollection, // Export a function that returns the user collection
+    getMeetingCollection: () => meetingCollection // Export a function that returns the meeting collection
 };
