248c0d6113c571b80d02dc268a1c76ff720c5dca
GaziMaksudur <gazimaksudur2@gmail.com>
2024-09-25 11:49:25 +0600

    jwt added

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 const cors = require('cors');
 const home = require('./routers/home');
 const userRoute = require('./routers/user');
+const jwt = require('jsonwebtoken');
 
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(
