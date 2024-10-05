9826908de27192d0d5d96dd91b463742cc2f10b1
minhaj.dev <minhajhasan.dev@gmail.com>
2024-10-06 02:15:21 +0600

    testing

--- routers/meeting.js
+++ routers/meeting.js
@@ -4,12 +4,13 @@ const {
   getMeetings,
   getMeetingById,
   joinmeeting,
- 
 } = require("../controllers/meetingController");
 const { verifyToken } = require("../services/middlewire");
 
-router.post("/create-meeting", verifyToken, createMeeting);
-router.get("/meetings", verifyToken, getMeetings);
+// router.post("/create-meeting", verifyToken, createMeeting);
+router.post("/create-meeting", createMeeting);
+// router.get("/meetings", verifyToken, getMeetings);
+router.get("/meetings", getMeetings);
 router.get("/meeting/:meetingId", getMeetingById); // TO DO - get meeting by id for delete and update/edit
 router.patch("/meeting/:meetingId", verifyToken, joinmeeting); //  update meeting by id
 
