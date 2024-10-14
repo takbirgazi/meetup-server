b6eb5873a5449551bbdc68f635cfa8cebc60c22f
GaziMaksudur <gazimaksudur2@gmail.com>
2024-10-14 23:14:20 +0600

    env changed

--- routers/home.js
+++ routers/home.js
@@ -1,15 +1,16 @@
-const { setToken, createToken } = require("../services/tokenService");
-
-const router = require("express").Router();
-
-router.get("/", (req, res) => {
-  res.send("MeetUp microservice");
-});
-
-router.post("/jwt", setToken);
-
-router.get("/getToken", createToken);
-// post Livekit token route
-router.post("/getToken", createToken);
-
+const { setToken, createToken } = require("../services/tokenService");
+
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.send("MeetUp microservice");
+});
+
+router.post("/jwt", setToken);
+
+router.get("/getToken", createToken);
+// post Livekit token route
+router.post("/getToken", createToken);
+
 module.exports = router;
+
